4945bdfa1ee16d7ac5b78e050e9e5bd7d3f11199
W-Herb <3219332909@qq.com>
2025-06-18 22:04:24 +0800

    开始吧

---
+++ src/main/java/com/crm/mxgl/MxglApplication.java
@@ -0,0 +1,13 @@
+package com.crm.mxgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MxglApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crm/mxgl/MxglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.mxgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
